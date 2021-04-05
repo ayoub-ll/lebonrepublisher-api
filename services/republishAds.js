361d9fbf8a1eb292e4911d561fd833bf11b6eed7
Syneed <ayoub.laarobi@gmail.com>
2021-04-05 23:28:39 +0200

    re-organize in folder services

--- services/republishAds.js
+++ services/republishAds.js
@@ -5,4 +5,6 @@ const config = require('config');
     var tokenn = await auth.main(config.get('lbc_username'), config.get('lbc_password')).then((value) => {
         console.log(value);
       });
+
+      
   })();
