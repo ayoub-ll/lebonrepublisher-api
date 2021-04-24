23925f36abddd505557f4360c3475d7314d62ffd
Syneed <ayoub.laarobi@gmail.com>
2021-04-24 04:18:56 +0200

    ok

--- http/api.js
+++ http/api.js
@@ -17,37 +17,37 @@ app.use(apikeyMiddleware.apiKeyMiddleware)
  * get bearer token LBC from LBC email & password
  */
 app.post('/auth', async (req, res) => {
-  let email = await req.body.email
-  let password = await req.body.password
+    let email = await req.body.email
+    let password = await req.body.password
 
-  if (!email || !password) {
-    res.status(401).json({ error: 'Email or password not found in request' })
-    res.send()
-  }
-
-  await auth.getToken(email, password).then((result) => {
-    if (result == 404) {
-      console.log('Account not found. Token promise timeout 30sec')
-      res.status(404).json({ error: 'Account not found' })
-      res.send()
-    } else {
-      const token = result.token
-      const accountId = result.accountId
-
-      console.log('token: ', token)
-      console.log('accountId: ', accountId)
-
-      if (!token) {
-        res.status(500).json({ error: 'Token null' })
+    if (!email || !password) {
+        res.status(401).json({error: 'Email or password not found in request'})
         res.send()
-      } else {
-        res.status(200)
-        res.send({ token, accountId })
-      }
     }
-  })
 
-  
+    await auth.getToken(email, password).then((result) => {
+        if (result == 404) {
+            console.log('Account not found. Token promise timeout 30sec')
+            res.status(404).json({error: 'Account not found'})
+            res.send()
+        } else {
+            const token = result.token
+            const accountId = result.accountId
+
+            console.log('token: ', token)
+            console.log('accountId: ', accountId)
+
+            if (!token) {
+                res.status(500).json({error: 'Token null'})
+                res.send()
+            } else {
+                res.status(200)
+                res.send({token, accountId})
+            }
+        }
+    })
+
+
 })
 
 /**
@@ -60,25 +60,30 @@ app.post('/auth', async (req, res) => {
  * - token
  */
 app.post('/getAds', async (req, res) => {
-  let accountId = await req.body.accountId
-  let token = await req.body.token
+    let accountId = await req.body.accountId
+    let token = await req.body.token
 
-  if (!accountId) {
-    res.status(400).json({ error: 'accountId not found' })
-    res.send()
-  }
+    if (!accountId) {
+        res.status(400).json({error: 'accountId not found'})
+        res.send()
+    }
 
-  if (!token) {
-    res.status(401).json({ error: 'token not found' })
-    res.send()
-  }
+    if (!token) {
+        res.status(401).json({error: 'token not found'})
+        res.send()
+    }
 
-  await getAdsService.getAds(token, accountId).then((result) => {
-    res.status(200)
-    res.send(result)
-  })
+    await getAdsService.getAds(token, accountId)
+        .then((result) => {
+            res.status(200)
+            res.send(result)
+        })
+        .catch((error) => {
+          res.status(500)
+          res.send()
+        })
 })
 
 app.listen(port, () => {
-  console.log(`LBP API listening at http://localhost:${port}`)
+    console.log(`LBP API listening at http://localhost:${port}`)
 })
